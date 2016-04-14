63c9b8b6bda0e2997b99f815082936222d02d174
AntonSava <sava310190@gmail.com>
2016-04-14 15:04:03 +0300

    изменение в импорте

--- app/src/main/java/ua/antonsava/firstapp/MainActivity.java
+++ app/src/main/java/ua/antonsava/firstapp/MainActivity.java
@@ -5,9 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
